19aaf98d319c8c4a0a7d84f87f34594c60bf9a44
Alfredo Garo <44888596+garomonegro@users.noreply.github.com>
2023-05-19 18:04:59 -0400

    example for generic templating

---
+++ examples/templating/generic/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	"github.com/keikoproj/kubedog/pkg/generic"
+)
+
+func main() {
+	TemplateArguments := []generic.TemplateArgument{
+		{
+			Key:                 "Namespace",
+			EnvironmentVariable: "KUBEDOG_EXAMPLE_NAMESPACE",
+			Mandatory:           false,
+			Default:             "kubedog-example",
+		},
+		{
+			Key:                 "Message",
+			EnvironmentVariable: "KUBEDOG_EXAMPLE_MESSAGE",
+			Mandatory:           false,
+			Default:             "Hello, Kubedog!",
+		},
+	}
+
+	args, err := generic.TemplateArgumentsToMap(TemplateArguments...)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	templatedInputConfigPath := "templates/pod.yaml"
+	_, err = generic.GenerateFileFromTemplate(templatedInputConfigPath, args)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
